e6811e723395fc94816b7c56b35fc8fca1dfd5a2
Raul Correia <raulvinicius245@gmail.com>
2023-07-10 17:43:54 -0300

    fix: EXTRA - Corrigido problemas de versões.

--- microservice-usuarios-auth/src/main/java/com/blx/usuariosauth/UsuariosAuthApplication.java
+++ microservice-usuarios-auth/src/main/java/com/blx/usuariosauth/UsuariosAuthApplication.java
@@ -2,9 +2,8 @@ package com.blx.usuariosauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+
 @SpringBootApplication
 public class UsuariosAuthApplication {
 
--- microservice-vendas/src/main/java/com/blx/vendas/clients/UsuarioClient.java
+++ microservice-vendas/src/main/java/com/blx/vendas/clients/UsuarioClient.java
@@ -1,17 +1,13 @@
 package com.blx.vendas.clients;
 
-
-//import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@Component
-//@FeignClient(name = "usuario", path = "${app.services.ms-usuarios}")
+
+@FeignClient(url = "${app.services.ms-usuarios}", name = "ms-usuarios", path = "/usuarios")
 public interface UsuarioClient {
 
-//    static final String PATH = "/usuarios";
-//
-//    @GetMapping("/exists/{usuarioId}")
-//    Boolean existsUsuarioById(@PathVariable Long usuarioId);
+    @GetMapping("/usuarios/exists/{usuarioId}")
+    Boolean existsUsuarioById(@PathVariable Long usuarioId);
 }
