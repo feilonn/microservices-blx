0337d8d241cd48395b34367cad1c0a434130735d
Raul Correia <raulvinicius245@gmail.com>
2023-07-12 15:29:41 -0300

    feat: 4.0 - Criado método para relatorio de produtos vendidos por usuario.

--- microservice-vendas/src/main/java/com/blx/vendas/repositories/VendasRespository.java
+++ microservice-vendas/src/main/java/com/blx/vendas/repositories/VendasRespository.java
@@ -9,6 +9,11 @@ import java.util.List;
 
 public interface VendasRespository extends JpaRepository<Vendas, Long> {
 
-    @Query(value = "select v.* from vendas v inner join usuarios u on u.id = v.usuario_id and u.id = :idUsuario", nativeQuery = true)
+    @Query(value = "select v.* from vendas v inner join usuarios u on " +
+            "u.id = v.usuario_id and u.id = :idUsuario", nativeQuery = true)
     List<Vendas> buscarComprasPorUsuario(Long idUsuario);
+
+    @Query(value = "select * from produtos p inner join vendas_produtos vp on p.id = vp.produto_id " +
+            "where p.usuario_id = :idUsuario", nativeQuery = true)
+    List<Produto> buscarProdutosVendidosPorUsuario(Long idUsuario);
 }
--- microservice-vendas/src/main/java/com/blx/vendas/services/RelatoriosService.java
+++ microservice-vendas/src/main/java/com/blx/vendas/services/RelatoriosService.java
@@ -1,7 +1,26 @@
 package com.blx.vendas.services;
 
+import com.blx.vendas.dtos.ProdutoResponse;
+import com.blx.vendas.mapper.ProdutoMapper;
+import com.blx.vendas.models.Produto;
+import com.blx.vendas.repositories.VendasRespository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class RelatoriosService {
+
+    private final VendasRespository respository;
+    private final ProdutoMapper produtoMapper;
+
+    public List<ProdutoResponse> buscarProdutosVendidosPorUsuario(Long usuarioId) {
+
+        List<Produto> produtos = respository.buscarProdutosVendidosPorUsuario(usuarioId);
+
+        return produtos.stream().map(produtoMapper::toProdutoResponse).collect(Collectors.toList());
+    }
 }
